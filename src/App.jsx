911cf6aabcb01d5f03e283ad0728e32fbf852d09
Ricardo Arturo <49619270+Ricaidito@users.noreply.github.com>
2022-09-26 14:42:58 -0400

    Fixed loading issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import { useEffect, useState } from "react";
 import Axios from "axios";
-import AOS from 'aos';
-
-import 'aos/dist/aos.css';
+import AOS from "aos";
 import Loadings from "./components/Loading";
 import PokemonCard from "./components/PokemonCard";
-
+import { Button, Stack } from "@mui/material";
+import "aos/dist/aos.css";
 import "./styles/main.scss";
 
 AOS.init();
 
 const App = () => {
   const [pokemon, setPokemon] = useState({});
-  const apiUrl = "https://pokeapi.co/api/v2/pokemon?limit=890";
-
+  const [apiUrl, setApiUrl] = useState("https://pokeapi.co/api/v2/pokemon");
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -27,17 +25,40 @@ const App = () => {
       })
       .then(() => {
         setLoading(false);
-      })  
-      
-  }, []);
+      });
+  }, [apiUrl]);
+
+  const nextPokemons = () => {
+    if (!pokemon.next) return;
+    setApiUrl(pokemon.next);
+  };
+
+  const prevPokemons = () => {
+    if (!pokemon.previous) return;
+    setApiUrl(pokemon.previous);
+  };
 
   return (
     <>
       {loading && <Loadings />}
+      <h1 className="poke-font" style={{ paddingLeft: "15px" }}>
+        Pokedex
+      </h1>
+      <Stack padding={4} spacing={2} direction="row">
+        <Button color="success" variant="contained" onClick={prevPokemons}>
+          Previous
+        </Button>
+        <Button color="secondary" variant="contained" onClick={nextPokemons}>
+          Next
+        </Button>
+      </Stack>
       <div className="pokemon-container">
-        {pokemon.results?.map((pokemon, index) => (
-          <PokemonCard key={index} index={index} pokemon={pokemon} />
-        ))}
+        {pokemon.results?.map(pokemon => {
+          const pokemonId = pokemon.url.split("/")[6];
+          return (
+            <PokemonCard key={pokemonId} id={pokemonId} pokemon={pokemon} />
+          );
+        })}
       </div>
     </>
   );
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,58 +1,55 @@
-import "../styles/PokemonCard.scss";
-
 import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import {useEffect} from "react";
+import { useEffect } from "react";
 import Axios from "axios";
+import "../styles/PokemonCard.scss";
 
-const PokemonCard = ({ index, pokemon }) => {
+const PokemonCard = ({ id, pokemon }) => {
   const { name } = pokemon;
 
-  const [info, setInfo] = React.useState({})
-  const infoAPI = `https://pokeapi.co/api/v2/pokemon/${index + 1}/`
+  const [info, setInfo] = React.useState({});
+  const infoAPI = `https://pokeapi.co/api/v2/pokemon/${id}/`;
 
   useEffect(() => {
     Axios.get(infoAPI)
-        .then(response => {
-          setInfo(response.data);
-        })
-        .catch(error => {
-          alert("Something went wrong!");
-          console.error(error);
-        });
-
-    }, [infoAPI]);
+      .then(response => {
+        setInfo(response.data);
+      })
+      .catch(error => {
+        alert("Something went wrong!");
+        console.error(error);
+      });
+  }, [infoAPI]);
 
   return (
     <div className="pokemon-container">
-      <span  data-aos="fade-up-right" data-aos-delay="300">
-        <Card variant="outlined" className="hover" style={{backgroundColor: "beige", transitionDuration: '0.5s'}}>
-        <CardContent>
-          <center>
-            <img
+      <span data-aos="fade-up-right" data-aos-delay="300">
+        <Card
+          variant="outlined"
+          className="hover"
+          style={{ backgroundColor: "beige", transitionDuration: "0.5s" }}
+        >
+          <CardContent>
+            <center>
+              <img
                 className="card-image"
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
-                    index + 1
-                }.png`}
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
                 alt={`${name}`}
-            />
-          </center>
-          <p className="small-text">
-            #{index + 1}
-          </p>
-          <p className="poke-font">
-            {name}
-          </p>
-          <p className="text-center desc">
-            weight: {info.weight}<br/>
-          </p>
-          <p className="text-center desc-blue">
-            height: {info.height}<br />
-            
-          </p>
-        </CardContent>
-      </Card>
+              />
+            </center>
+            <p className="small-text">#{id}</p>
+            <p className="poke-font">{name}</p>
+            <p className="text-center desc">
+              weight: {info.weight}
+              <br />
+            </p>
+            <p className="text-center desc-blue">
+              height: {info.height}
+              <br />
+            </p>
+          </CardContent>
+        </Card>
       </span>
     </div>
   );
